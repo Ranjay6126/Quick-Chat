649a504362f6877414c111ecfd32046aad9076d8
Ranjay6126 <6126ranjayprajapati@gmail.com>
2025-11-01 00:39:38 +0530

    create the userprofile

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,29 +1,27 @@
-import { useState } from "react"
-import ChatContainer from "../components/ChatContainer"
-import RightSidebar from "../components/RightSidebar"
-import SideBar from "../components/SideBar"
-
+import { useState } from "react";
+import ChatContainer from "../components/ChatContainer";
+import RightSidebar from "../components/RightSidebar";
+import SideBar from "../components/SideBar";
 
 const HomePage = () => {
-
-
-  const[selectedUser, setSelectedUser] = useState(false)
+  const [selectedUser, setSelectedUser] = useState(false);
 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
-      
-      <div className={`backdrop-blur-xl boder-2 border-gray-500 rounded-2xl
-      overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 
-      'md:grid-cols-[1fr_1.5fr_1fr]':'md:grid-cols-2'}`}> 
-    
-        <SideBar/>
-        <ChatContainer/>
-        <RightSidebar/>
-
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-500 rounded-2xl
+        overflow-hidden h-full grid grid-cols-1 relative ${
+          selectedUser
+            ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+            : "md:grid-cols-2"
+        }`}
+      >
+        <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
