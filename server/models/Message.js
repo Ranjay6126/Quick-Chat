3aa7f6f44a8f145f9a47d2050c3ee277dd48e3ed
Ranjay6126 <6126ranjayprajapati@gmail.com>
2025-11-06 22:44:57 +0530

    all most backend completed

--- server/models/Message.js
+++ server/models/Message.js
@@ -4,12 +4,12 @@ const messageSchema = new mongoose.Schema({
 
     senderId:{type:mongoose.Schema.Types.ObjectId, ref:"User", required:true},
     receiverId:{type:mongoose.Schema.Types.ObjectId, ref:"User", required:true},
-    text:{type: String,},
-    image:{type: String,},
+    text:{type: String},
+    image:{type: String},
     seen:{type : Boolean, default:false}
 
 }, {timestamps:true});
 
-const message = mongoose.model("User", MessageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
